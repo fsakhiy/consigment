1fb8d37b9a913a5bd31f5fe03b4fabb9f0fc646c
fairuz <sakhiyfairuz@gmail.com>
2024-09-14 18:13:29 +0700

    fix url for auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { getSession } from '@auth0/nextjs-auth0';
+import { redirect } from 'next/navigation';
 
-export default async function ProfileServer() {
+export default async function HomePage() {
   const session = await getSession();
 
   // Type guard to check for session and user
   if (!session || !session.user) {
-    return <div>Please <a href="http://localhost:3000/api/auth/login">sign in</a> to view your profile.</div>;
+    // return <div>Please <a href="http://localhost:3000/api/auth/login">sign in</a> to view your profile.</div>;
+    redirect('/api/auth/login')
   }
 
   // Type assertion to ensure that `session.user` is defined
@@ -16,7 +18,7 @@ export default async function ProfileServer() {
       <img src={user.picture} alt={user.name} />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
-      <a href='http://localhost:3000/api/auth/logout'>log out</a>
+      <a href='/api/auth/logout'>log out</a>
     </div>
   );
 }
