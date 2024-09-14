afd0af3846c5a5b552c15b861a6391e155a908f6
fairuz <sakhiyfairuz@gmail.com>
2024-09-14 18:53:49 +0700

    added auth0 and gravatar img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+                protocol: 'https',
+                hostname: 's.gravatar.com',
+                pathname: '/avatar/**', // Allow Gravatar avatars
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.auth0.com',
+                pathname: '/avatars/**', // Allow Auth0 avatar fallback images
+            },
+        ],
+    },
+
+};
 
 export default nextConfig;
