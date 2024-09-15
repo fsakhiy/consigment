d9c9ecbf0ac7c84e32bdbaa5bc00c7840d1f27c0
fairuz <sakhiyfairuz@gmail.com>
2024-09-15 22:15:34 +0700

    added server actions for creating a product

--- app/dashboard/products/action.ts
+++ app/dashboard/products/action.ts
@@ -1,12 +1,37 @@
-'use server'
-import prisma from '@/lib/prisma'
-import { revalidatePath } from 'next/cache'
+"use server";
+import { productFormSchema } from "@/components/forms/products";
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
 
 const getProducts = async () => {
-    revalidatePath('/dashboard/products')
-    const data = await prisma.product.findMany();
-    
-    return data
-}
+  revalidatePath("/dashboard/products");
+  const data = await prisma.product.findMany();
 
-export { getProducts }
+  return data;
+};
+
+const createProduct = async (values: z.infer<typeof productFormSchema>) => {
+  try {
+    const data = await prisma.product.create({
+      data: {
+        name: values.name,
+        description: values.description,
+        sku: values.sku,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          // SKU already exists
+          throw new Error('SKU already exists');
+        }
+      }
+      throw new Error('Something went wrong');
+  
+  }
+};
+export { getProducts, createProduct };
