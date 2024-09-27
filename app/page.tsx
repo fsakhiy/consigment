046dec4b01ce3b47d3c5df7a8aef380d01ab21f0
fairuz <sakhiyfairuz@gmail.com>
2024-09-28 05:50:55 +0700

    added auto redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <div>
-      welcome to a stock consignment app!
+  redirect('/dashboard')
+  // return (
+  //   <div>
+  //     welcome to a stock consignment app!
 
-      <Link className="underline" href={'/dashboard'}>click here go get started!</Link>  
-    </div>
-  )
+  //     <Link className="underline" href={'/dashboard'}>click here go get started!</Link>  
+  //   </div>
+  // )
 }
