1cf52746d2d6b59dab6ac17ce628a3e68c022896
fairuz <sakhiyfairuz@gmail.com>
2024-09-28 07:07:35 +0700

    added delete button

---
+++ components/delete-button.tsx
@@ -0,0 +1,42 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import React from "react";
+
+interface DeleteProps {
+    fn: () => void
+}
+
+const DeleteButton: React.FC<DeleteProps> = ({fn}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button>Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={fn}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export {DeleteButton}
